d8baac1d2f31bba142651ec6d1221e62b8cf72b2
githubanushka <98322178+githubanushka@users.noreply.github.com>
2023-05-07 23:17:03 +0530

    forgetpassword java file

--- app/src/main/java/com/example/mynotes/forgetpassword.java
+++ app/src/main/java/com/example/mynotes/forgetpassword.java
@@ -2,13 +2,51 @@ package com.example.mynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class forgetpassword extends AppCompatActivity {
+    private EditText mforgetpassword;
+    private Button mpasswordrecoverbutton;
+    private TextView mgobacktologin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgetpassword);
+
+        mforgetpassword=findViewById(R.id.forgetpassword);
+        mpasswordrecoverbutton=findViewById(R.id.passwordrecoverbutton);
+        mgobacktologin=findViewById(R.id.gobacktologin);
+
+        mgobacktologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new intent(forgetpassword.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        mpasswordrecoverbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail=mforgetpassword.getText().toString().trim();
+                if(mail.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(),"Enter your mail first : ",Toast.LENGTH_SHORT).show();
+
+                }
+                else
+                {
+                    // We have to send the password recover mail
+                }
+            }
+        });
     }
 }
